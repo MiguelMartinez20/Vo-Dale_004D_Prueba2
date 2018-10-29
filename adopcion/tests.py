5aa3990cfe04ca299c4dadcc581593cb084458ca
MiguelMartinez20 <mig.martinezm@alumnos.duoc.cl>
2018-10-29 19:41:49 +0000

    Agrega Pruebas unitarias y Corrige ultimos 3 errores de W3C. Modifica CSS

--- adopcion/tests.py
+++ adopcion/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 # Create your tests here.
+
+class TestViews(TestCase):
+
+    def testRegisterView(self):
+        # Arrange
+        name_view = 'register_view'
+        path = reverse(name_view)
+        expected = 'register_view'
+        result = 'a'
+
+        #Act
+        result = resolve(path).view_name
+
+        #Assert
+        assert result == expected
+
+    def testAdoptionView(self):
+        #Arrange
+        name_view = 'dog_detail'
+        path = reverse(name_view, kwargs={'pk': 1})
+        expected = 'dog_detail'
+        result = 'a'
+
+        #Act
+        result = resolve(path).view_name
+
+        #Assert
+        assert result == expected
+
